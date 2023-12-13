4640824618348ead6fcffa463810d05b9d45c293
gregoriosup <guilherme_gregorio@usp.br>
2023-12-13 08:09:17 -0300

    13122023

--- validacao.R
+++ validacao.R
@@ -37,24 +37,24 @@ t.test(rec_temp1, mu=100)
 #### linearidade ####
 
 
-conc <- c(2.5,
-          5,
-          10,
-          15,
-          20,
-          25,
-          30,
-          35,
-          40)
-abs <- c(0.057,
-         0.1043333333,
-         0.1963333333,
-         0.2933333333,
-         0.4093333333,
-         0.5123333333,
-         0.6153333333,
-         0.712,
-         0.8183333333)
+conc <- c(2.53,
+          5.05,
+          10.11,
+          15.16,
+          20.21,
+          25.26,
+          30.32,
+          35.37,
+          40.42)
+abs <- c(0.052,
+         0.107,
+         0.199,
+         0.294,
+         0.392,
+         0.481,
+         0.580,
+         0.676,
+         0.772)
 
 df_lin <- data.frame(conc, abs)
 
@@ -63,16 +63,34 @@ mod <- lm(df_lin)
 anova(mod)
 
 anova(mod) #teste F <0,05 nao rejeita sig do mod
+
 par(mfrow=c(2,2))
 plot(mod) #homocedasticidade e residuos
 par(mfrow=c(1,1))
+
 bptest(mod) #homocedasticidade
+
 shapiro.test(mod$residuals) #normalidade dos residuos >0,05 é normal
+
 boxplot(mod$residuals) #outlier dos residuos
 boxplot(df_lin$abs)
+
 summary(mod)
+
 cor(df_lin$conc, df_lin$abs, method = "pearson")
+
 plot(df_lin$conc, df_lin$abs) #verificar linearidade
 
 
+# Supondo que 'lm_model' é o seu modelo de regressão linear
+residuals <- residuals(mod)
+
+# Calcular a função de autocorrelação e autocorrelação parcial dos resíduos
+acf_res <- acf(residuals)
+pacf_res <- pacf(residuals)
+
+# Plotar os resultados
+par(mfrow=c(2,1))
+plot(acf_res, main="Função de Autocorrelação dos Resíduos")
+plot(pacf_res, main="Função de Autocorrelação Parcial dos Resíduos")
 
